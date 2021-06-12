2b8389b6e5396279209e8611480fc0d5f80bf486
Manas Agarwal <your.email@your-place.com>
2021-06-12 17:57:06 +0530

    Initial Commit

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.project_index, name="project_index"),
+    path("home/", views.home, name="home"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
+]
